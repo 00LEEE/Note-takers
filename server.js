dfd0108b9a040c5062bdaa8c116defa530badf62
Kole Dallam <kolejasonD@gmail.com>
2023-03-02 19:20:31 -0700

    adding notes route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/notes.js');
+const notesRoute = require('./routes/notes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
