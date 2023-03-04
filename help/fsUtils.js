413a12074adc9edf7806c6d941479ac67f1647a0
Kole Dallam <kolejasonD@gmail.com>
2023-03-03 23:08:39 -0700

    Utils fix

--- help/fsUtils.js
+++ help/fsUtils.js
@@ -1,36 +1,23 @@
 const fs = require('fs').promises;
 
-const writeToFile = async (destination, content) => {
-    try {
-        await fs.writeFile(destination, JSON.stringify(content, null, 4));
-        console.info(`\nData written to ${destination}.`);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-const readAndAppend = async (content, file) => {
-    try {
-        const data = await fs.readFile(file, 'utf8');
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        await writeToFile(file, parsedData);
-    } catch (error) {
-        console.error(error);
-    }
-};
-
-module.exports = { fs, writeToFile, readAndAppend };
-
-const fs = require('fs').promises;
-
-async function writeToFile(file, data) {
+async function readJSONFile(filePath) {
   try {
-    await fs.writeFile(file, JSON.stringify(data, null, 4));
-    console.log(`Data successfully written to ${file}`);
-  } catch (error) {
-    console.error(`Error writing to ${file}: ${error}`);
+    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const data = JSON.parse(fileContent);
+    return data;
+  } catch (err) {
+    throw new Error(`Error reading ${filePath}: ${err.message}`);
   }
 }
 
-module.exports = { writeToFile };
+async function writeJSONFile(filePath, data) {
+  try {
+    const fileContent = JSON.stringify(data, null, 2);
+    await fs.writeFile(filePath, fileContent, 'utf-8');
+  } catch (err) {
+    throw new Error(`Error writing to ${filePath}: ${err.message}`);
+  }
+}
+
+module.exports = { readJSONFile, writeJSONFile };
+
